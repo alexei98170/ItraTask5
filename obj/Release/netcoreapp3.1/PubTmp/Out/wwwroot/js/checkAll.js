0b3e96cec652f5837539b6f1e33012101fda957a
Алексей Гулецкий <a9805@mail.ru>
2021-04-15 16:19:26 +0300

    123

---
+++ obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/checkAll.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $(".checkboxClas").click(function () {
+        data = $(this).attr("data-number");
+        if ($(this).is(":checked")) {
+            url = "Users/SelectAll/1";
+            $('body input:checkbox').prop('checked', true);
+        }
+        else {
+            url = "Users/SelectAll/2";
+            $('body input:checkbox').prop('checked', false);
+        }
+        $.ajax({
+            type: "POST",
+            url: url
+        });
+    });
+});
